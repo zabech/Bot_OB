9a158b59aba92c408241ab69e30c605225fcabac
zabech <izabyh@gmail.com>
2026-07-26 08:20:53 +0800

    Add zones_now function with NotImplementedError

---
+++ zones.py
@@ -0,0 +1,5 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+async def zones_now(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    raise NotImplementedError("zones_now belum dipindahkan")
