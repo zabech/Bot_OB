9b8cc360af3b2c7cee36d1012b4ae0fff53bafb0
zabech <izabyh@gmail.com>
2026-08-09 17:59:13 +0700

    Change REQUIRE_FVG environment variable to true

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ ATR_MULTIPLIER = float(os.environ.get("ATR_MULTIPLIER", "1.5"))          # SL = 
 
 # Konfigurasi deteksi OB tingkat lanjut
 REQUIRE_BOS = os.environ.get("REQUIRE_BOS", "true").lower() == "true"
-REQUIRE_FVG = os.environ.get("REQUIRE_FVG", "false").lower() == "true"
+REQUIRE_FVG = os.environ.get("REQUIRE_FVG", "true").lower() == "true"
 MITIGATION_50PCT = os.environ.get("MITIGATION_50PCT", "true").lower() == "true"
 SWING_LOOKBACK = int(os.environ.get("SWING_LOOKBACK", "10"))
 
